f1cde5af4cd07c4b201020300270148686c14695
TeacherLoc <zZminhrauZz@gmail.com>
2024-12-05 20:34:36 +0700

    a

---
+++ quanlichitieu-main/quanlichitieu-main/QLchitieu/app/src/main/java/com/example/qlchitieu/fragment/BudgetFragment.kt
@@ -0,0 +1,8 @@
+package com.example.qlchitieu.fragment
+
+import androidx.fragment.app.Fragment
+import com.example.qlchitieu.R
+
+class BudgetFragment : Fragment(R.layout.fragment_budget) {
+    // Các xử lý của Fragment Ngân sách
+}
